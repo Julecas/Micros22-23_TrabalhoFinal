9bad5cbcfe8715cc6aeecdd1a6d22a8a579c9d4e
MartimAgostinho <martim.d.agostinho@protonmail.com>
2022-11-18 10:47:33 +0000

    print_status

---
+++ status.asm
@@ -0,0 +1,90 @@
+print_status proc
+        
+        push ax
+        push cx
+        push dx
+        push si
+        push di
+        push bx
+        
+        xor dx , dx   ;= 0
+            
+        mov AH , 2  ;cursor na posicao 0,0    
+        mov bh , 0
+        INT 10h
+        
+        mov si , offset status_str
+        mov bl , 0
+        call printf
+        
+        ;xor dx , dx         ;nao tenho a certeza se e preciso
+        mov dx , GENnCHAR   ; cursor nos numeros
+        mov ah , 2
+        mov bh , 0
+        int 10h
+        
+        mov ax , gen_num
+        mov cx , Ndigitos
+        call print_int
+        
+        ;mov cx , CELLnCHAR
+        mov dx , CELLnCHAR
+        mov ah , 2
+        mov bh , 0
+        int 10h
+        
+        mov ax , cell_num
+        mov cx , Ndigitos
+        call print_int
+        
+        pop bx
+        pop di
+        pop si
+        pop dx       
+        pop cx
+        pop ax
+        ret  
+    endp 
+    
+    ;Ax = num
+    ;cx = numero de char a escrever
+    print_int proc
+        
+        push bp     ;[bp - 2] -> numero de char
+        mov bp,sp
+        
+        push cx
+        push dx 
+        push bx
+        
+        mov bx , 10 
+        
+        loop1_prtint:  
+        
+            xor dx , dx    
+            div bx
+            add dl , '0'
+            push dx
+        
+        dec cx
+        jz endlp1_prtint
+        or ax,ax
+        jnz loop1_prtint
+        
+        endlp1_prtint:
+        
+        add cx , [bp - 2]   ;numero de char para dar print 
+        
+        loop2_prtint:
+            
+            pop ax
+            call co
+            dec cx
+        jnz loop2_prtint        
+        
+        pop bx
+        pop dx
+        pop cx
+        pop bp
+        ret               
+    endp 
