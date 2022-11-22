4396e3305e266c2f36b53b971d97616d1ee50ce0
MartimAgostinho <martim.d.agostinho@protonmail.com>
2022-11-22 14:10:14 +0000

    mouse release

--- sleep_com_keypress.asm
+++ sleep_com_keypress.asm
@@ -49,3 +49,66 @@
         ret
         
     endp
+
+
+        ;bx = numero de segundos  
+    ;para o programa x segundos
+    ;Provavelmente tem comportamentos estranhos no emulador
+    ;incerteza -> tempo de espera existe ]bx - 1(seg) +- (incerteza do interrupt) , bx [
+    sleep_key_press proc
+        
+        push cx 
+        push dx
+        push ax
+        push bx
+             
+        mov ah , 2ch
+        
+        int 21h
+        mov al , dh
+        
+        lp1_slpkp:
+                
+                push dx
+                push ax 
+                
+                mov ah , 6
+                mov dl , 255
+                int 21h     
+                                                      
+                pop ax
+                pop dx
+                
+                jnz fim_cslp    ;verifica por keypress
+                
+                int 21h
+        
+            cmp dh , al     ;enquanto estiver no msm segundo repete
+            je lp1_slpkp
+            push cx 
+            push ax
+            
+            dec bx
+            
+            mov ax , bx
+            mov cx , 1
+            call print_int
+            
+            pop ax
+            pop cx  
+            or dx , dx
+            jz fim_slpkp
+            
+                int 21h 
+                mov al , dh
+                jmp lp1_slpkp
+            
+        fim_slpkp:
+        
+        pop bx
+        pop ax
+        pop dx 
+        pop cx
+        ret
+        
+    endp
